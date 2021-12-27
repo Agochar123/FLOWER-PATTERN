1d8d7000dd7d92b743b38e3e45019e7cda788d8a
Agochar123 <94464716+Agochar123@users.noreply.github.com>
2021-12-27 22:35:32 +0530

    Add files via upload

---
+++ Student_Project.py
@@ -0,0 +1,14 @@
+import turtle
+
+#Keep the background color as black
+turtle.speed(10)
+n=1
+colors=['red','green','blue','yellow','purple']
+
+for angle in range(#Write the range):
+    #Increment the variable n by 1 units
+    #Write the if block to check whether n=5
+        #Set n variable value to -1
+    turtle.color(#Pass lists elements one by one)
+    turtle.seth(angle)
+    turtle.circle(100)
